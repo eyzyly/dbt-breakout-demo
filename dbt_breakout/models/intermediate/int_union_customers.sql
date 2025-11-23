7352886c637553e7cae995a23f01a0e633579870
Zulkifly,Eddy <eddyarief@hotmail.com>
2025-11-23 22:26:13 +0000

    clean up nulls

--- dbt_breakout/models/intermediate/int_union_customers.sql
+++ dbt_breakout/models/intermediate/int_union_customers.sql
@@ -1,3 +1,20 @@
-{{ dbt_utils.union_relations(
+With unioned_customers as (
+    {{ dbt_utils.union_relations(
     relations=[ref('stg_customers'), ref('new_customer_demo')],
 ) }}
+)
+
+Select 
+    _dbt_source_relation as source,
+    customer_id,
+    first_name,
+    last_name,
+    CASE
+        WHEN email IS NULL OR email = '' THEN 'Unknown'
+        ELSE email
+    END AS email,
+    CASE 
+        WHEN industry IS NULL OR industry = '' THEN 'Unknown' 
+        ELSE industry 
+    END AS industry
+from unioned_customers
