9f39e72ea16a062c7243a6b04570f694b12b754f
Zulkifly,Eddy <eddyarief@hotmail.com>
2025-11-23 16:14:25 +0000

    refactor limit data

--- dbt_breakout/macros/limit_data_in_dev.sql
+++ dbt_breakout/macros/limit_data_in_dev.sql
@@ -1,7 +1,13 @@
-{% macro limit_data_in_dev(sample_size = 1000) -%}
-    {% if target.name in ['personal'] -%}
-        LIMIT {{sample_size}}
+{% macro limit_data_in_dev(
+        ref_date='created_at',
+        lookback_window_days=3,
+        fixed_date='2018-04-09'
+    ) -%}
+
+    {% if target.name in ['personal'] %}
+        {{ ref_date }} >= date('{{ fixed_date }}') - interval {{ lookback_window_days }} day
     {% else %}
         TRUE
-    {%- endif %}
+    {% endif %}
+
 {%- endmacro %}
