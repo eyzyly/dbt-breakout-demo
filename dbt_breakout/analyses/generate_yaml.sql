d43852789e53f2455d9797a6a95ef101624fb6e0
Zulkifly,Eddy <eddyarief@hotmail.com>
2025-11-24 02:29:10 +0000

    important config

--- dbt_breakout/analyses/generate_yaml.sql
+++ dbt_breakout/analyses/generate_yaml.sql
@@ -1,5 +1,6 @@
 -- Write macro that generates model docs for dim_customer
 -- Link: https://hub.getdbt.com/dbt-labs/codegen/latest/
+-- This will come in helpful: https://docs.getdbt.com/reference/resource-configs/persist_docs
 {{ codegen.generate_model_yaml(
     model_names=['dim_customer']
 ) }}
